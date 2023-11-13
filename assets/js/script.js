fa994d8c4407c2689a4523ec4082701208b522d0
sabri chafroud <chafroud.sabri@hotmail.fr>
2023-11-13 16:59:34 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,77 @@
+console.log("code charge");
+
+                        
+
+
+                                // #################################### Methode recuperation donnee ########################################  
+
+// const keyMaster = "$2a$10$sIvMxOzunGBB5JXAmSeYjO.3ZdozgJXu7oXypD4UO9GP16Sx6ATBm";
+// const apiUrl = "https://api.jsonbin.io/v3";
+// const binId = "654e055b12a5d37659977757";
+// let dataFetchJson;
+
+
+
+// await getDataFetchBis();
+// console.log("Voici les données via fetch: ", dataFetchJson.record);
+
+// async function getDataFetchBis () {
+//     const res = await fetch(apiUrl+"/b/"+binId, {
+//         method: 'GET',
+//         headers :{
+//             'X-Master-Key': keyMaster,
+//         }
+//     });
+//     dataFetchJson = await res.json(); 
+// }
+
+
+
+
+                                // #################################### Methode envoie donnee ###############################################
+
+const keyMaster = "$2a$10$sIvMxOzunGBB5JXAmSeYjO.3ZdozgJXu7oXypD4UO9GP16Sx6ATBm";
+const apiUrl = "https://api.jsonbin.io/v3";
+const binId = "654e055b12a5d37659977757";
+let dataFetchJson;
+const joueurs = {
+    "joueurs" : [
+        {
+            nom: "zinedine",
+            prenom: "zidane",
+            poste: "milieu de terrain"
+        },
+        {
+            nom: "ronaldo",
+            prenom: "cristiano",
+            poste: "milieu gauche"
+        },
+        {
+            nom: "ronaldo",
+            prenom: "nazaret",
+            poste: "attaquant"
+        },
+        {
+            nom: "ramos",
+            prenom: "sergio",
+            poste: "defenseur"
+        },
+    ]                            
+}                               
+                                
+getDataFetchBis();
+// console.log("Voici les données via fetch: ", dataFetchJson.record);
+                                
+async function getDataFetchBis () {
+    const res = await fetch(apiUrl+"/b", {
+        method: 'POST',
+        headers :{
+            'X-Master-Key': keyMaster,
+            'content-Type': 'application/json',
+    },
+        body: JSON.stringify(joueurs),
+    });
+    dataFetchJson = await res.json(); 
+    return dataFetchJson;
+}
+                                
