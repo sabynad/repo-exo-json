7a08ef4497c6f92a8bdf2d3bc5506610b3aba745
sabri chafroud <chafroud.sabri@hotmail.fr>
2023-11-13 22:49:42 +0100

    13/11/23 22H48

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ console.log("code charge");
                         
 
 
-                                // #################################### Methode recuperation donnee ########################################  
+                                // #################################### Methode recuperation donnee (GET) ########################################  
 
 // const keyMaster = "$2a$10$sIvMxOzunGBB5JXAmSeYjO.3ZdozgJXu7oXypD4UO9GP16Sx6ATBm";
 // const apiUrl = "https://api.jsonbin.io/v3";
@@ -28,7 +28,7 @@ console.log("code charge");
 
 
 
-                                // #################################### Methode envoie donnee ###############################################
+                                // #################################### Methode envoie donnee (POST)###############################################
 
 const keyMaster = "$2a$10$sIvMxOzunGBB5JXAmSeYjO.3ZdozgJXu7oXypD4UO9GP16Sx6ATBm";
 const apiUrl = "https://api.jsonbin.io/v3";
@@ -61,7 +61,7 @@ const joueurs = {
                                 
 getDataFetchBis();
 // console.log("Voici les données via fetch: ", dataFetchJson.record);
-                                
+ 
 async function getDataFetchBis () {
     const res = await fetch(apiUrl+"/b", {
         method: 'POST',
@@ -74,4 +74,23 @@ async function getDataFetchBis () {
     dataFetchJson = await res.json(); 
     return dataFetchJson;
 }
-                                
+
+
+//  let formulaire = document.querySelector("form");
+// let btnEnvoyer = document.querySelector("btn");
+
+// formulaire.addEventListener("submit", async (event) => {
+//     event.preventDefault();
+//     let resultat = btnEnvoyer.value;
+
+// console.log("valeur boutton submit est : ", resultat);
+// let dataFetchJson = await getDataFetchBis();
+//   console.log("Voici les données via fetch: ", dataFetchJson.record);
+// });
+
+
+
+
+
+
+
